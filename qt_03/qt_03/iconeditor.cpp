c598140fb5873f9b1c8aeb3dd8cb10dbca4dec47
CnLzh <cnlzh1516@outlook.com>
2022-03-13 23:51:40 +0800

    update

--- qt_03/qt_03/iconeditor.cpp
+++ qt_03/qt_03/iconeditor.cpp
@@ -48,5 +48,23 @@ void iconEditor::setZoomFactor(int newZoom)
 
 void iconEditor::paintEvent(QPaintEvent *event)
 {
-
+    QPainter painter(this);
+    if(zoom >= 3){
+        painter.setPen(palette().foreground().color());//？
+        for(int i = 0; i <= image.width(); i++)
+            painter.drawLine(zoom * i,0,zoom * i, zoom * image.height());
+        for(int j =  0; j <= image.height(); j++)
+            painter.drawLine(0, zoom * j, zoom * image.width(), zoom * j);
+    }
+    for(int i = 0; i < image.width(); i++){
+        for(int j = 0; j < image.height(); j++){
+            QRect rect = pixelRect(i, j);
+            if(!event->region().intersect().isEmpyt()){
+                QColor color = QColor::fromRgba(image.pixel(i,j));
+                if(color.alpha() < 255)
+                    painter.fillRect(rect,Qt::white);
+                painter.fillRect(rect,color);
+            }
+        }
+    }
 }
